2549137e91d23d0eb7bdc14ee2d8ca5b00398722
kristru <krissafuentes@gmail.com>
2017-12-30 18:17:39 -0700

    import the router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BroweserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BroweserRouter>
+    , document.getElementById('root'));
+
 registerServiceWorker();
