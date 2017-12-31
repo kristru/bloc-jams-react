964ed68f8ac034d4ebe710dc53b7a34713790999
kristru <krissafuentes@gmail.com>
2017-12-30 18:48:01 -0700

    add links and components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BroweserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -8,7 +8,7 @@ import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BroweserRouter>
+  </BrowserRouter>
     , document.getElementById('root'));
 
 registerServiceWorker();
