a00f98024aa63ad5ff3a345a088fe1a992fdba7d
kristru <krissafuentes@gmail.com>
2017-12-30 18:38:08 -0700

    create landing and library components

---
+++ src/components/Landing.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Landing = () => (
+  <section className="landing">
+    This is where the Landing page will go.
+  </section>
+)
+
+export default Landing;
---
+++ src/components/Library.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Library = () => (
+  <section className="library">
+    This is where the Library page goes
+  </section>
+)
+
+export default Library;
