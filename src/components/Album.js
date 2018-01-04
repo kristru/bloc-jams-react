d4e2bd88cd5d51f854ce4ecf58c60553e7540bde
kristru <krissafuentes@gmail.com>
2018-01-03 19:21:56 -0700

    displaying album info and image

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import albumData from './../data/albums';
 
 class Album extends Component{
-    render(){
-      return (
-        <section className="album">
-          {this.props.match.params.slug} Album will go here
+  constructor(props) {
+    super(props);
+
+  const album = albumData.find( album => {
+    console.log(album, this.props);
+    return album.slug === this.props.match.params.slug
+  });
+
+    this.state = {
+      album: album
+    };
+  }
+
+  render() {
+    return (
+      <section className="album">
+        <section>
+          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
+          <div className="album-details">
+            <h1 id="album-title">{this.state.album.title}</h1>
+            <h2 className="artist">{this.state.album.artist}</h2>
+            <div id="release-info">{this.state.album.releaseInfo}</div>
+          </div>
         </section>
-      );
-    }
+      </section>
+    );
+  }
 }
 
 export default Album;
