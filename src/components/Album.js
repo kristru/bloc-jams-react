39dd2bd63e8e8dd8b53c01a1921ea0857d88854a
kristru <krissafuentes@gmail.com>
2018-01-07 15:36:47 -0700

    add play-pause and prev playbar button function

--- src/components/Album.js
+++ src/components/Album.js
@@ -46,6 +46,14 @@ class Album extends Component{
     }
   }
 
+handlePrevClick(song){
+  const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+  const newIndex = Math.max(0, currentIndex - 1);
+  const newSong = this.state.album.songs[newIndex];
+  this.setSong(newSong);
+  this.play(newSong);
+}
+
   render() {
     return (
       <section className="album">
@@ -81,7 +89,12 @@ class Album extends Component{
               }
           </tbody>
         </table>
-        <PlayerBar />
+          <PlayerBar
+            isPlaying={this.state.isPlaying}
+            currentSong={this.state.currentSong}
+            handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+            handlePrevClick={() => this.handlePrevClick()}
+          />
       </section>
     );
   }
